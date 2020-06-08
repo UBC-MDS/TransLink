e3937dbf75d06d4e25c070f27a80edb8aecef83f
Simardeep Kaur <simardeep.kaur.jeji@gmail.com>
2020-06-08 16:25:53 -0600

    Updated wordcloud according to Business requirements

--- src/claim_analysis.R
+++ src/claim_analysis.R
@@ -9,8 +9,11 @@ library(wordcloud)
 library(leaflet)
 library(DT)
 library(dplyr)
+library(ggwordcloud)
+library(wordcloud2)
+library(PubMedWordcloud)
 
-display.brewer.all()
+
 ui <- dashboardPage(
     dashboardHeader(title = "Reasons for Incidents"),
     dashboardSidebar(collapsed = TRUE, disable = TRUE),
@@ -36,21 +39,24 @@ server <- function(input, output) {
     # read location data
     location_data <- read_excel("../data/TransLink Raw Data/Claim_colour_df.xlsx")
     verb_data <- read_excel("../data/TransLink Raw Data/verb_colour_df.xlsx")
-    
+    view(location_data)
     # leaflet map
     output$mymap <- renderLeaflet({
         # Show first 20 rows from the `quakes` dataset
         leaflet() %>%
             addProviderTiles("CartoDB.Positron") %>%
-            addCircleMarkers(lng=location_data$long, lat=location_data$latt, radius=4,fillOpacity=1, color = location_data$impact_colour )
-    })
-    #print(location_data$impact_colour)
+            addCircleMarkers(lng=location_data$long, lat=location_data$latt, radius=4,fillOpacity=1, color = location_data$impact_colour, popup = paste0("<b>", location_data$Claim_Desc, "</b> <br>", "<b>", "Date: ", "</b> ", location_data$Date, "<br>", 
+                                                                                                                                                         "<b>","Bus Year: ", "</b> ", location_data$Bus_Route_Code, "<br>", 
+                                                                                                                                                         "<b>","Manufacturer: ", "</b> ", location_data$Vehicle_Number, "<br>" ) )
+    })   
     
     output$my_verb_map <- renderLeaflet({
         # Show first 20 rows from the `quakes` dataset
         leaflet() %>%
             addProviderTiles("CartoDB.Positron") %>%
-            addCircleMarkers(lng=verb_data$long, lat=verb_data$latt, radius=4,fillOpacity=1, color = verb_data$verb_colour)
+            addCircleMarkers(lng=verb_data$long, lat=verb_data$latt, radius=4,fillOpacity=1, color = verb_data$verb_colour,  popup = paste0("<b>", location_data$Claim_Desc, "</b> <br>", "<b>", "Date: ", "</b> ", location_data$Date, "<br>", 
+                                                                                                                                            "<b>","Bus Year: ", "</b> ", location_data$Bus_Route_Code, "<br>", 
+                                                                                                                                            "<b>","Manufacturer: ", "</b> ", location_data$Vehicle_Number, "<br>" ))
     })
     
     # plot for Impacts
@@ -67,21 +73,48 @@ server <- function(input, output) {
             
             wc_df_impact <- df %>% select(impact, impact_colour)
             impact_df <- count(wc_df_impact, impact,impact_colour )
-            view(impact_df)
+            #view(impact_df)
             
             
             #basecolors = rainbow(length(unique(group)))
             
             
-            
-            wordcloud(impact_df$impact, impact_df$n, max.words = 300, ordered.colors=TRUE,random.color=FALSE,min.freq = 1,
-                      colors = as.character(impact_df$impact_colour), scale=c(4,0.25))
-            
+          #   wordcloud_rep <- repeatable(wordcloud)
+          # wordcloud_rep(impact_df$impact, impact_df$n, max.words = 300, size = 1.5,ordered.colors=TRUE,random.color=FALSE,min.freq = 1,
+          #            colors = as.character(impact_df$impact_colour), scale=c(4,0.5))
+          #wordcloud(impact_df, color = impact_df$impact_colour)
+            ggplot(
+                impact_df,
+                aes(
+                    label = impact, size = n,
+                    color = as.character(impact_colour)
+                )
+            ) +
+                geom_text_wordcloud_area() +
+                scale_colour_identity()+
+                scale_size_area(max_size = 24) +
+                theme_minimal()
             
             
             
         } else
-            wordcloud(impact_df$impact, impact_df$n,ordered.colors=TRUE, random.color=FALSE,max.words = 300,min.freq = 1, colors = as.character(impact_df$impact_colour), scale=c(4,0.25))
+           #  wordcloud_rep <- repeatable(wordcloud)
+           # wordcloud_rep(impact_df$impact, impact_df$n,ordered.colors=TRUE,size = 1.5,
+           #            random.color=FALSE,max.words = 300,min.freq = 1, colors = as.character(impact_df$impact_colour), scale=c(4,0.25))
+           #  #wordcloud2(impact_df, color = impact_df$impact_colour)
+        
+              ggplot(
+                impact_df,
+                aes(
+                    label = impact, size = n,
+                    color = impact_colour,
+
+                )
+            ) +
+            geom_text_wordcloud_area() +
+            scale_colour_identity()+
+            scale_size_area(max_size = 24) +
+            theme_minimal()
     })
     
     # plot for Impacts
@@ -111,18 +144,36 @@ server <- function(input, output) {
             
             
             verb_df <- count(wc_verb_df, chosen_verb_x, verb_colour)
-            #final_verb_df <- inner_join(verb_df,df, by= 'chosen_verb_y' )
-            #view(verb_df)
-            
-            #png("wordcloud.png", width=1280,height=800) 
-            #png("wordcloud_packages.png", width=12,height=10, units='in', res=300)
-            wordcloud(verb_df$chosen_verb_x, verb_df$n, max.words = 100, ordered.colors=TRUE,random.color=FALSE,min.freq = 1,
-                      colors = verb_df$verb_colour)
-                  
+            ggplot(
+                verb_df,
+                aes(
+                    label = chosen_verb_x, size = n,
+                    color = verb_colour,
+                    
+                )
+            ) +
+                geom_text_wordcloud_area() +
+                scale_colour_identity()+
+                scale_size_area(max_size = 24) +
+                theme_minimal()
+        
             
         } else
-            wordcloud(verb_df$words, verb_df$n, max.words = 100, ordered.colors=TRUE,random.color=FALSE,min.freq = 1,
-                      colors = verb_df$verb_colour)
+            
+            verb_df <- count(wc_verb_df, chosen_verb_x, verb_colour)
+        ggplot(
+            verb_df,
+            aes(
+                label = chosen_verb_x, size = n,
+                color = verb_colour,
+                
+            )
+        ) +
+            geom_text_wordcloud_area() +
+            scale_colour_identity()+
+            scale_size_area(max_size = 24) +
+            theme_minimal()
+        
     })
 }
 shinyApp(ui = ui, server = server)
