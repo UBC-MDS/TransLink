bec0e57663ef86040504f454b28f50843ab59fab
Simardeep Kaur <simardeep.kaur.jeji@gmail.com>
2020-06-09 21:21:23 -0600

    Added dropdown to show the changes on the map

--- src/claim_analysis.R
+++ src/claim_analysis.R
@@ -13,33 +13,41 @@ library(ggwordcloud)
 library(wordcloud2)
 library(PubMedWordcloud)
 
+location_data <- read_excel("../data/TransLink Raw Data/Claim_colour_df.xlsx")
+verb_data <- read_excel("../data/TransLink Raw Data/verb_colour_df.xlsx")
+
+
 
 ui <- dashboardPage(
     dashboardHeader(title = "Reasons for Incidents"),
     dashboardSidebar(collapsed = TRUE, disable = TRUE),
- dashboardBody(
     
-         fluidRow(
+    dashboardBody(
+        
+        fluidRow(
             
-                 box(leafletOutput("mymap", height = 250)),
-                    box(plotOutput("plot", height = 250), title = "NOUN")
-         ),
-         fluidRow(
-                      box(leafletOutput("my_verb_map", height = 250)),
-           
-                   box(plotOutput("plot_verb", height = 250), title = "VERB")
-             
-         )
-     )
-     
- )
+            box(leafletOutput("mymap", height = 250)),
+            box(plotOutput("plot", height = 250), title = "NOUN")
+        ),
+        fluidRow(
+            # box(leafletOutput("my_verb_map", height = 250)),
+            box(uiOutput("frequent_impacts")),
+            
+            #box(plotOutput("plot_verb", height = 250), title = "VERB")
+            box(leafletOutput("my_updated_map", height = 250))
+            
+        )
+    )
+    
+)
 
 server <- function(input, output) {
     
     # read location data
     location_data <- read_excel("../data/TransLink Raw Data/Claim_colour_df.xlsx")
     verb_data <- read_excel("../data/TransLink Raw Data/verb_colour_df.xlsx")
-    view(location_data)
+    
+    #view(location_data)
     # leaflet map
     output$mymap <- renderLeaflet({
         # Show first 20 rows from the `quakes` dataset
@@ -59,7 +67,11 @@ server <- function(input, output) {
                                                                                                                                             "<b>","Manufacturer: ", "</b> ", location_data$Vehicle_Number, "<br>" ))
     })
     
+    
+    
+    
     # plot for Impacts
+    
     output$plot = renderPlot({
         if (isTruthy(input$mymap_bounds)) {
             bounds = input$mymap_bounds
@@ -68,21 +80,10 @@ server <- function(input, output) {
                 between(latt, bounds$south, bounds$north)
             )
             
-            
-            
-            
             wc_df_impact <- df %>% select(impact, impact_colour)
             impact_df <- count(wc_df_impact, impact,impact_colour )
             #view(impact_df)
             
-            
-            #basecolors = rainbow(length(unique(group)))
-            
-            
-          #   wordcloud_rep <- repeatable(wordcloud)
-          # wordcloud_rep(impact_df$impact, impact_df$n, max.words = 300, size = 1.5,ordered.colors=TRUE,random.color=FALSE,min.freq = 1,
-          #            colors = as.character(impact_df$impact_colour), scale=c(4,0.5))
-          #wordcloud(impact_df, color = impact_df$impact_colour)
             ggplot(
                 impact_df,
                 aes(
@@ -98,17 +99,13 @@ server <- function(input, output) {
             
             
         } else
-           #  wordcloud_rep <- repeatable(wordcloud)
-           # wordcloud_rep(impact_df$impact, impact_df$n,ordered.colors=TRUE,size = 1.5,
-           #            random.color=FALSE,max.words = 300,min.freq = 1, colors = as.character(impact_df$impact_colour), scale=c(4,0.25))
-           #  #wordcloud2(impact_df, color = impact_df$impact_colour)
-        
-              ggplot(
+            
+            ggplot(
                 impact_df,
                 aes(
                     label = impact, size = n,
-                    color = impact_colour,
-
+                    color = impact_colour
+                    
                 )
             ) +
             geom_text_wordcloud_area() +
@@ -116,6 +113,18 @@ server <- function(input, output) {
             scale_size_area(max_size = 24) +
             theme_minimal()
     })
+    
+    dataInput <- reactive(
+        
+        if (isTruthy(input$my_verb_map_bounds)){
+            bounds = input$my_verb_map_bounds
+            verb_data %>% filter(
+                between(long, bounds$west, bounds$east),
+                between(latt, bounds$south, bounds$north)
+            )
+        }
+    )
+    
     
     # plot for Impacts
     output$plot_verb = renderPlot({
@@ -127,19 +136,7 @@ server <- function(input, output) {
             )
             
             
-            
-            minilist= list()
-            for(i in df$chosen_verb_y){
-                if(str_contains(i,',')){
-                    minilist <- c(minilist,(strsplit(i, ',')))
-                }
-                else{
-                    minilist <- c(minilist, i)
-                    
-                }
-            }
-            
-            wc_verb_df = df %>% select(chosen_verb_x, verb_colour)
+            wc_verb_df = dataInput() %>% select(chosen_verb_x, verb_colour)
             #tibble(chosen_verb_y = as.character(flatten(minilist)))
             
             
@@ -148,7 +145,7 @@ server <- function(input, output) {
                 verb_df,
                 aes(
                     label = chosen_verb_x, size = n,
-                    color = verb_colour,
+                    color = verb_colour
                     
                 )
             ) +
@@ -156,24 +153,73 @@ server <- function(input, output) {
                 scale_colour_identity()+
                 scale_size_area(max_size = 24) +
                 theme_minimal()
-        
+            
             
         } else
+            
             
             verb_df <- count(wc_verb_df, chosen_verb_x, verb_colour)
         ggplot(
             verb_df,
             aes(
                 label = chosen_verb_x, size = n,
-                color = verb_colour,
+                color = verb_colour
                 
             )
         ) +
             geom_text_wordcloud_area() +
             scale_colour_identity()+
-            scale_size_area(max_size = 24) +
+            scale_size_area(max_size =24)+
             theme_minimal()
         
     })
+    # changing the map according to selected options
+    dat <- reactive({
+        selected_impact <- input$frequent_impacts
+        if (isTruthy(input$mymap_bounds)) {
+            bounds = input$mymap_bounds
+            df <- location_data %>% filter(
+                between(long, bounds$west, bounds$east),
+                between(latt, bounds$south, bounds$north)
+            )}
+        #sub_data <-
+        df %>% filter(df$impact == selected_impact)
+    })
+    #view(sub_data)  
+    
+    
+    output$my_updated_map <- renderLeaflet({
+        # Show first 20 rows from the `quakes` dataset
+        
+        
+        
+        
+        leaflet("my_updated_map", data = dat()) %>% 
+            addProviderTiles("CartoDB.Positron") %>%
+            addCircleMarkers(lng=dat()$long, lat=dat()$latt, radius=4,fillOpacity=1, 
+                             color = dat()$verb_colour,
+                             popup = paste0("<b>",  dat()$Claim_Desc, "</b> <br>", "<b>", "Date: ", "</b> ",  dat()$Date, "<br>",
+                                            "<b>","Bus Year: ", "</b> ",  dat()$Bus_Route_Code, "<br>",
+                                            "<b>","Manufacturer: ", "</b> ",  dat()$Vehicle_Number, "<br>" ))
+        
+    })
+    output$frequent_impacts <- renderUI({
+        if (isTruthy(input$mymap_bounds)) {
+            bounds = input$mymap_bounds
+            df <- location_data %>% filter(
+                between(long, bounds$west, bounds$east),
+                between(latt, bounds$south, bounds$north)
+            )
+            view(df)
+            wc_df_impact <- df %>% select(impact, impact_colour)
+            impact_df <- count(wc_df_impact, impact,impact_colour )
+            sorted_df <- impact_df[order(-impact_df$n), ]
+            
+            
+        }
+        
+        selectInput("frequent_impacts", "Most frequently Impacted objects:",as.character(sorted_df$impact)[1:5])
+    })
+    
 }
 shinyApp(ui = ui, server = server)
