bb74766aca6c50d28948c8e296d836a16dc8708f
braydentang1 <brayden.tang1@gmail.com>
2020-06-09 22:18:58 -0600

    revise code to take into account implied dist of bus run times.

--- src/ml-model/2_model_xgb.py
+++ src/ml-model/2_model_xgb.py
@@ -4,10 +4,15 @@ import datetime
 import multiprocessing
 
 from lightgbm import LGBMClassifier
-from shap import TreeExplainer, summary_plot
-from hyperopt import hp, tpe, Trials, fmin, STATUS_OK
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import roc_auc_score
+
+from shap import TreeExplainer, summary_plot
+from hyperopt import hp, tpe, Trials, fmin, STATUS_OK
 
 # Join 
 train = pd.read_csv("data/ml-model/train.csv")
@@ -78,36 +83,6 @@ def preprocessor(X, newdata):
     validation['season'] = [seasons_lookup[month] for month in validation['month']]
     validation['part_of_day'] = [day_lookup[hour] for hour in validation['hour']]
 
-    all_categorical = train.select_dtypes(include=['object']).columns.tolist()
-    # all_categorical_catboost = [i for i in all_categorical if i not in ['date', 'bus_no', 'day_of_week', 'experience_category', 'city', 'bus_carry_capacity', 'season', 'part_of_day', 'month']]
-
-    all_categorical_regular = [i for i in all_categorical if i not in ['date', 'bus_no']]
-    all_categorical_regular.extend(['month', 'hour', 'bus_carry_capacity'])
-
-    # # CatBoost encoder
-    # catboost_encoder = ce.CatBoostEncoder(
-    #     cols=all_categorical_catboost,
-    #     random_state=200350623)
-
-    # train = catboost_encoder.fit_transform(
-    #     X=train,
-    #     y=train['incident'])
-
-    # validation = catboost_encoder.transform(
-    #     X=validation)
-    
-    reverse_lookup_reg = {}
-    # Integer encoding for LightGBM's built in handling of such features
-    for variable in all_categorical_regular:
-        train[variable] = train[variable].astype('category')
-        reverse_lookup_reg[variable] = dict(enumerate(train[variable].cat.categories))
-        train[variable] = train[variable].cat.codes
-
-        validation[variable] = validation[variable].astype('category')
-        validation[variable] = validation[variable].cat.codes
-
-    # Log plus 1 transformation for some of the right skewed variables
-
     train['total_rain'] = [np.log1p(x) for x in train['total_rain']]
     train['total_snow'] = [np.log1p(x) for x in train['total_snow']]
     train['wind_spd'] = [np.log1p(x) for x in train['wind_spd']]
@@ -121,21 +96,24 @@ def preprocessor(X, newdata):
     train['rel_hum'] = [np.sqrt(x) for x in train['rel_hum']]
     validation['rel_hum'] = [np.sqrt(x) for x in validation['rel_hum']]
 
-    train = train.drop(columns=['date', 'time', 'total_precip', 'bus_no', 'empl_id'])
-    validation = validation.drop(columns=['date', 'time', 'total_precip', 'bus_no', 'empl_id'])
+    train = train.drop(columns=['date', 'total_precip', 'time', 'bus_no', 'empl_id', 'day_of_year'])
+    validation = validation.drop(columns=['date', 'total_precip', 'time', 'bus_no', 'empl_id', 'day_of_year'])
 
-    return {'train': train, 'newdata': validation, 'lookup_reg': reverse_lookup_reg}
+    return {'train': train, 'newdata': validation}
 
 def optimize_param(train, space, all_categorical, n_splits=5):
 
     bayes_trials = Trials()
     folds = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=200350623)
+
+    train_temp = train.drop(columns=['incident'])
+    y = train['incident']
+
     def objective(space):
         
         # n_estimators is huge because we use early_stopping here!
-        model = LGBMClassifier(
+        model = LGBClassifier(
             n_estimators=5000,
-            learning_rate=0.01,
             num_leaves=space['num_leaves'],
             max_depth=space['max_depth'],
             colsample_bytree=space['colsample_bytree'],
@@ -146,36 +124,53 @@ def optimize_param(train, space, all_categorical, n_splits=5):
             random_state=200350623
         )
 
+        all_numeric = list(set(train_temp.columns.tolist()) - set(all_categorical))
+
+        numeric_transformer = Pipeline(steps=[
+            ('imputer', SimpleImputer(strategy='median'))
+        ])
+
+        categorical_transformer = Pipeline(steps=[
+            ('imputer', SimpleImputer(strategy='most_frequent', fill_value='missing')),
+            ('onehot', OneHotEncoder(sparse=False, handle_unknown='ignore'))
+        ]
+        )
+
+        transformer = ColumnTransformer(transformers=[
+            ('num', numeric_transformer, all_numeric),
+            ('cat', categorical_transformer, all_categorical)],
+            remainder='passthrough'
+        )
+        
         auroc_scores = []
         best_iter = []
-        for train_idx, val_idx in folds.split(X=train, y=train['incident']):
-            X_train_temp = train.iloc[train_idx, :].drop(columns=['incident'])
-            y_train_temp = train['incident'][train_idx]
-            X_val_temp = train.iloc[val_idx, :].drop(columns=['incident'])
-            y_val_temp = train['incident'][val_idx]
+        for train_idx, val_idx in folds.split(X=train_temp, y=y):
+            X_train_temp = train_temp.iloc[train_idx, :]
+            y_train_temp = y[train_idx]
+            X_val_temp = train_temp.iloc[val_idx, :]
+            y_val_temp = y[val_idx]
+
+            X = transformer.fit_transform(X=X_train_temp)
+            X_val = transformer.transform(X=X_val_temp)
 
             model.fit(
-                X=X_train_temp,
-                y=y_train_temp,
-                early_stopping_rounds=20,
-                eval_set=[(X_val_temp, y_val_temp)],
-                eval_metric='auc',
-                verbose=50,
-                feature_name=X_train_temp.columns.tolist(),
-                categorical_feature=all_categorical
-                )
+                X=X,
+                y=y_train_temp, 
+                eval_metric="auc",
+                eval_set=[(X_val, y_val_temp)],
+                early_stopping_rounds=20
+            )
 
             auroc_scores.append(
-                model._best_score['valid_0']['auc']
+                model.best_score
             )
             
             best_iter.append(
-                model.best_iteration_
-            )
-        
+                model.best_iteration
+            )        
         return {'loss': -np.mean(auroc_scores), 'params': space, 'status': STATUS_OK, 'iterations': np.round(np.median(best_iter), 0)}
 
-    fmin(fn=objective, space=space, algo=tpe.suggest, max_evals=50, trials=bayes_trials, rstate=np.random.RandomState(200350623))
+    fmin(fn=objective, space=space, algo=tpe.suggest, max_evals=80, trials=bayes_trials, rstate=np.random.RandomState(200350623))
     return bayes_trials
 
 
@@ -185,13 +180,15 @@ preprocessor_trained = preprocessor(
 
 train_set = preprocessor_trained['train']
 test_set = preprocessor_trained['newdata']
-all_categorical_regular = list(preprocessor_trained['lookup_reg'].keys())
+
+all_categorical_vars = train_set.select_dtypes('object').columns.tolist()
+all_categorical_vars.extend(['hour', 'bus_carry_capacity', 'month'])
 
 space = {
     'max_depth': hp.choice('max_depth', np.arange(1, 15, dtype=int)),
     'colsample_bytree': hp.uniform('colsample_bytree', 0.15, 1),
     'reg_alpha': hp.uniform('reg_alpha', 0, 15),
-    'num_leaves': hp.choice('num_leaves', np.arange(5, 500, dtype=int)),
+    'num_leaves': hp.choice('num_leaves', np.arange(5, 900, dtype=int)),
     'reg_lambda': hp.uniform('reg_lambda', 0, 40)
     }
 
@@ -199,14 +196,14 @@ results = optimize_param(
     train=train_set,
     n_splits=5,
     space=space,
-    all_categorical=all_categorical_regular)
+    all_categorical=all_categorical_vars)
 
 best_model_params = results.best_trial['result']['params']
 n_iter = results.best_trial['result']['iterations']
 
 final_lgb = LGBMClassifier(
     n_estimators=int(n_iter),
-    learning_rate=0.01,
+    learning_rate=0.05,
     num_leaves=best_model_params['num_leaves'],
     max_depth=best_model_params['max_depth'],
     colsample_bytree=best_model_params['colsample_bytree'],
@@ -232,7 +229,7 @@ test_roc = roc_auc_score(y_true=test_set['incident'], y_score=test_scores[:, 1])
 
 feat_import = TreeExplainer(final_lgb).shap_values(X=train_set.drop(columns=['incident']))
 
-summary_plot(feat_import, train_set.drop(columns=['incident']))
+summary_plot(feat_import, train_set.drop(columns=['incident']), plot_type="bar")
 
 pd.DataFrame({
     'var': train_set.drop(columns=['incident']).columns.tolist(),
--- src/ml-model/R/1_sample.R
+++ src/ml-model/R/1_sample.R
@@ -43,35 +43,115 @@ source("src/ml-model/R/helper-scripts/build_series.R")
 #' )
 main <- function(path_accident_data, path_weather_stations_data_hour, path_weather_stations_data_day, path_out) {
   
-  if (!str_detect(path_accident_data, ".rds")) {
-    stop("path_accident_data should be a specific .rds file.")
-  } else if (!str_detect(path_weather_stations_data_hour, ".rds")) {
-    stop("path_weather_stations_data_hour should be a specific .rds file.")
-  } else if (!str_detect(path_weather_stations_data_day, ".rds")) {
-    stop("path_weather_stations_data_day should be a specific .rds file.")
-  } else if (str_detect(path_out, "\\.xlsx$|\\.csv$|\\.rds$|//.txt$")) {
-    stop("The output path must be a general path, not a 
-             path to a specific file. Remove the file extension.")
-  } else if (endsWith(path_out, "/")) {
-    stop("path_out should not end with /")
-  }
-  
-  # Read in data produced by 0_get-weather-data.R
-  all_data <- readRDS(path_accident_data) %>%
-    mutate(day_of_year = yday(loss_date))
-  
-  # Read in weather data by location, per hour and per day
-  stations_per_loc_hour <- readRDS(path_weather_stations_data_hour)
-  stations_per_loc_day <- readRDS(path_weather_stations_data_day)
-  # Randomly select rows, with replacement, from the entire dataset
-  set.seed(200350623)
-  sample_rows <- sample_n(all_data, size = round(nrow(all_data) * 3.3, 0), replace = TRUE)
-  
-  # Specify the total possible values we could possibly change 
-  all_lines <- unique(all_data$line_no)
-  all_days <- 1:366
-  all_hours <- 0:23
-  
+        if (!str_detect(path_accident_data, ".rds")) {
+          stop("path_accident_data should be a specific .rds file.")
+        } else if (!str_detect(path_weather_stations_data_hour, ".rds")) {
+          stop("path_weather_stations_data_hour should be a specific .rds file.")
+        } else if (!str_detect(path_weather_stations_data_day, ".rds")) {
+          stop("path_weather_stations_data_day should be a specific .rds file.")
+        } else if (str_detect(path_out, "\\.xlsx$|\\.csv$|\\.rds$|//.txt$")) {
+          stop("The output path must be a general path, not a 
+                   path to a specific file. Remove the file extension.")
+        } else if (endsWith(path_out, "/")) {
+          stop("path_out should not end with /")
+        }
+        
+        # Read in data produced by 0_get-weather-data.R and group old lines with their replacement if any/remove
+        # lines not in service anymore/Skytrains
+        all_data <- readRDS(path_accident_data) %>%
+          mutate(day_of_year = yday(loss_date)) %>%
+          mutate(line_no = case_when(
+            line_no == "135" ~ "95",
+            line_no == "C21" ~ "23",
+            line_no == "C1" ~ "131",
+            line_no == "C15" ~ "215",
+            line_no == "C20" ~ "70",
+            line_no == "C23" ~ "23",
+            line_no == "C5" ~ "148",
+            line_no == "154" ~ "128",
+            line_no == "999" ~ NA_character_,
+            line_no == "N16" ~ "16",
+            line_no == "C98" ~ "418",
+            line_no == "97" ~ NA_character_,
+            line_no == "991" ~ NA_character_,
+            line_no == "895" ~ NA_character_,
+            line_no == "C75" ~ "322",
+            line_no == "C71" ~ "371",
+            line_no == "C50" ~ "360",
+            line_no == "C70" ~ "370",
+            line_no == "C73" ~ "373",
+            line_no == "590" ~ NA_character_,
+            line_no == "C88" ~ "618",
+            line_no == "C25" ~ "181",
+            line_no == "C94" ~ "414",
+            line_no == "C93" ~ "413",
+            line_no == "C96" ~ "416",
+            line_no == "C7" ~ "147",
+            line_no == "C6" ~ "146",
+            line_no == "C9" ~ "109",
+            line_no == "177" ~ NA_character_,
+            line_no == "C27" ~ "183",
+            line_no == "C30" ~ "189",
+            line_no == "C37" ~ "171",
+            line_no == "C38" ~ "173",
+            line_no == "C29" ~ "187",
+            line_no == "C28" ~ "184",
+            line_no == "C43" ~ "743",
+            line_no == "C40" ~ "175",
+            line_no == "C44" ~ "744",
+            line_no == "C47" ~ "733",
+            line_no == "C26" ~ "182",
+            line_no == "C24" ~ "180",
+            line_no == "C76" ~ "310",
+            line_no == "C86" ~ "616",
+            line_no == "C53" ~ "363",
+            TRUE ~ line_no
+          )) %>%
+          drop_na(line_no)
+          
+        
+        # Read in weather data by location, per hour and per day
+        stations_per_loc_hour <- readRDS(path_weather_stations_data_hour)
+        stations_per_loc_day <- readRDS(path_weather_stations_data_day)
+        # Randomly select rows, with replacement, from the entire dataset
+        set.seed(200350623)
+        sample_rows <- sample_n(all_data, size = round(nrow(all_data) * 3.3, 0), replace = TRUE)
+        
+        all_combinations <- read_csv("data/TransLink Raw Data/Scheduled_Actual_services_2019.csv", na = c("NULL", -1), col_types = cols(line_no = col_character(), bus_number = col_character())) %>%
+          drop_na(line_no) %>%
+          mutate(block = case_when(
+            month(sheet_from_date) == 12 ~ 1,
+            month(sheet_from_date) == 4 ~ 2,
+            month(sheet_from_date) == 6 ~ 3,
+            TRUE ~ 4
+          )) %>%
+          mutate(start_time_hour = hour(seconds_to_period(scheduled_trip_start_time)),
+                 end_time_hour = hour(seconds_to_period(sch_trip_end_time)),
+                 day_start = yday(sheet_from_date),
+                 day_end = yday(sheet_to_date)) %>% 
+          group_by(block, line_no, day_type_code, start_time_hour, end_time_hour) %>%
+          summarise() %>%
+          mutate(max_hr = ifelse(end_time_hour == 0, 0, pmax(start_time_hour, end_time_hour))) %>%
+          group_by(block, day_type_code, line_no, max_hr) %>%
+          summarise() %>%
+          rename("start_time_hour" = max_hr)
+        
+        lookup_r <- tibble(
+          r_bus = c("R1", "R2", "R4", "R5"),
+          old = c("96", "239", "43", "95")
+        )
+    
+    all_r_bus <- bind_rows(pmap(.l = list(x = lookup_r$r_bus, y = lookup_r$old), .f = function(x, y) {
+      all_combinations %>%
+        filter(line_no == {{y}}) %>%
+        ungroup() %>%
+        mutate(line_no = rep(x, nrow(.)))
+    }))
+    
+    all_combinations <- all_combinations %>%
+      bind_rows(., all_r_bus)
+    
+    
   # Generate a vector indicating what variable to change 
   # 1 is line, 2 is hour, 3 is day of year. 
   # Sampling scheme taken from this article here: https://medium.com/geoai/using-machine-learning-to-predict-car-accident-risk-4d92c91a7d57
@@ -81,18 +161,7 @@ main <- function(path_accident_data, path_weather_stations_data_hour, path_weath
   set.seed(200350623)
   sample_rows_with_var <- sample_rows %>%
     mutate(var_to_change = sample.int(n = 3, size = nrow(sample_rows), replace = TRUE))
-  
-  # Get count of scenarios
-  counts <- sample_rows_with_var %>%
-    group_by(var_to_change) %>%
-    count()
-  
-  # Generate the replacement values for each observation sampled above, drawn uniformly and randomly
-  set.seed(200350623)
-  lines <- sample(all_lines, size = counts$n[which(counts$var_to_change == 1)], replace = TRUE)
-  hours <- sample(all_hours, size = counts$n[which(counts$var_to_change == 2)], replace = TRUE)
-  days <- sample(all_days, size = counts$n[which(counts$var_to_change == 3)], replace = TRUE)
-  
+
   all_negative_samples <- vector("list", 3)
   
   for (i in 1:3) {
@@ -103,15 +172,34 @@ main <- function(path_accident_data, path_weather_stations_data_hour, path_weath
       # overwrite the line with the randomly generated ones. Then, check
       # if the observation exists as a recorded accident by joining. If "no", 
       # then keep the synthetic observation otherwise throw it away.
-      
-      all_negative_samples[[1]] <- sample_rows_with_var %>%
+    
+      set.seed(200350623)
+      temp <- sample_rows_with_var %>%
         filter(var_to_change == 1) %>%
-        mutate(line_no = lines) %>%
+        mutate(week_day = case_when(
+          wday(loss_date) %in% c(2, 3, 4, 5, 6) ~ "MF",
+          wday(loss_date) %in% c(7) ~ "SAT",
+          TRUE ~ "SUN"
+        )) %>%
+        mutate(row_num = seq(1, nrow(.)),
+               block = case_when(
+                  day_of_year >= 1 & day_of_year <= 111 ~ 1,
+                  day_of_year >= 112 & day_of_year <= 174 ~ 2,
+                  day_of_year >= 175 & day_of_year <= 244 ~ 3,
+                  TRUE ~ 4)) %>%
+        left_join(., all_combinations, by = c("week_day" = "day_type_code", "hour_of_loss" = "start_time_hour", "block")) %>%
+        group_by(row_num) %>%
+        sample_n(size = 1) %>%
+        select(-line_no.x, -block, -week_day) %>%
+        rename("line_no" = line_no.y) %>%
+        ungroup() %>%
         left_join(all_data %>% transmute(line_no, hour_of_loss, day_of_year, check = "yes")) %>%
         replace_na(list(check = "no")) %>%
         filter(check == "no") %>%
         mutate(target = rep(0, nrow(.))) %>%
-        select(-var_to_change, -check)
+        select(-var_to_change, -check, -time_of_loss, -row_num)
+      
+      all_negative_samples[[1]] <- temp
       
     } else if (i == 2) {
       
@@ -119,14 +207,32 @@ main <- function(path_accident_data, path_weather_stations_data_hour, path_weath
       # to change the other variables that also change by the hour - so the weather variables.
       weather_vars <- c("pressure", "rel_hum", "elev","temp", "visib", "wind_dir", "wind_spd")
       
+      set.seed(200350623)
       temp <- sample_rows_with_var %>%
         filter(var_to_change == 2) %>%
-        mutate(hour_of_loss = hours) %>%
+        mutate(week_day = case_when(
+          wday(loss_date) %in% c(2, 3, 4, 5, 6) ~ "MF",
+          wday(loss_date) %in% c(7) ~ "SAT",
+          TRUE ~ "SUN"
+        )) %>%
+        mutate(row_num = seq(1, nrow(.)),
+               block = case_when(
+                 day_of_year >= 1 & day_of_year <= 111 ~ 1,
+                 day_of_year >= 112 & day_of_year <= 174 ~ 2,
+                 day_of_year >= 175 & day_of_year <= 244 ~ 3,
+                 TRUE ~ 4)) %>%
+        left_join(., all_combinations, by = c("week_day" = "day_type_code", "line_no", "block")) %>%
+        group_by(row_num) %>%
+        sample_n(size = 1) %>%
+        select(-block, -week_day, -hour_of_loss) %>%
+        rename(hour_of_loss = start_time_hour) %>%
+        ungroup() %>%
+        mutate(hour_of_loss = ifelse(is.na(hour_of_loss), sample(c(0:1, 5:23), 1), hour_of_loss)) %>%
         left_join(all_data %>% transmute(line_no, hour_of_loss, day_of_year, check = "yes")) %>%
         replace_na(list(check = "no")) %>%
         filter(check == "no") %>%
         mutate(target = rep(0, nrow(.))) %>%
-        select(-all_of(weather_vars), -check, -var_to_change)
+        select(-var_to_change, -check, -time_of_loss, -row_num, -all_of(weather_vars))
       
       all_locations <- unique(temp$city_of_incident)
       
@@ -149,15 +255,28 @@ main <- function(path_accident_data, path_weather_stations_data_hour, path_weath
       weather_vars <- c("pressure", "rel_hum", "elev","temp", "visib", "wind_dir", "wind_spd")
       weather_vars_day <- c("total_precip", "total_rain", "total_snow")
       
+      set.seed(200350623)
       temp <- sample_rows_with_var %>%
         filter(var_to_change == 3) %>%
-        mutate(day_of_year = days,
-               loss_date = as.Date(day_of_year, origin = paste(year(loss_date) - 1, 12, 31, sep = "-"))) %>%
+        mutate(row_num = seq(1, nrow(.))) %>%
+        left_join(., all_combinations, by = c("line_no", "hour_of_loss" = "start_time_hour")) %>%
+        group_by(row_num) %>%
+        sample_n(size = 1) %>%
+        mutate(block = ifelse(is.na(block), sample(1:4, size = 1), block)) %>%
+        select(-day_type_code) %>%
+        mutate(day_of_year = case_when(
+          block == 1 ~ sample(1:111, 1),
+          block == 2 ~ sample(112:174, 1),
+          block == 3 ~ sample(175:244, 1),
+          TRUE ~ sample(176:366, 1)
+        )) %>%
+        ungroup() %>%
+        mutate(loss_date = as.Date(day_of_year, origin = paste(year(loss_date) - 1, 12, 31, sep = "-"))) %>%
         left_join(all_data %>% transmute(line_no, hour_of_loss, day_of_year, check = "yes")) %>%
         replace_na(list(check = "no")) %>%
         filter(check == "no") %>%
         mutate(target = rep(0, nrow(.))) %>%
-        select(-all_of(weather_vars), -all_of(weather_vars_day), -check, -var_to_change)
+        select(-var_to_change, -check, -time_of_loss, -block, -row_num, -all_of(weather_vars_day), -all_of(weather_vars))
       
       set.seed(200350623)
       all_2020 <- temp %>%
@@ -205,11 +324,13 @@ main <- function(path_accident_data, path_weather_stations_data_hour, path_weath
     bind_rows(all_data, .) %>%
     rename(
       date = loss_date,
-      time = time_of_loss,
       hour = hour_of_loss,
       city = city_of_incident,
       incident = target
-    )
+    ) %>%
+    select(-time_of_loss) %>%
+    mutate(bus_age = as.numeric(bus_age),
+           bus_carry_capacity = as.numeric(bus_carry_capacity))
     
   write_csv(all_samples_combined, paste0(path_out, "/final_data_combined.csv"))
   
