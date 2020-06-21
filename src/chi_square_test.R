ae447d1b774edb7d9b3c4870a34d1c88777d8278
Simardeep Kaur <simardeep.kaur.jeji@gmail.com>
2020-06-20 18:45:04 -0600

    Deleting unwanted files and creating a folder for claim analysis

--- src/chi_square_test.R
+++
@@ -1,9 +0,0 @@
-library(dplyr)
-library(tidyverse)
-library(readxl)
-collision_table <- read_excel("../data/TransLink Raw Data/Updated_tags_df.xlsx")
-collision_table$month <- format(as.Date(collision_table$`Date of Incident`), "%m")
-input_string = 'mirror'
-collision_table <- collision_table %>% mutate(impact_test = ifelse(impact == input_string, input_string, paste('not_',input_string)))
-chi_test_data <- table(collision_table$impact_test, collision_table$month)
-chisq.test(chi_test_data)
